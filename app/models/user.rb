5d31c4377c7959614b62bd364a45b29d1fbcca95
Thet Hmuu <thethmuueinsoe@gmail.com>
2020-10-07 17:47:02 +0630

    Added Devise Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
     has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
     has_many :following, through: :active_relationships, source: :followed
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index]
+  
   get 'comments/create'
+  
   resources :blogs do
     resources :comments
   end
-  resources :users, only: [:index]
+  
+  root "blogs#index"
+
+  resources :relationships, only: [:create, :destroy]
 end
